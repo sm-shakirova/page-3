bc0351f62f9f6886377b9879bf3da05245b334ee
Sofia <smshakirova@edu.hse.ru>
2022-01-30 11:32:56 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,25 +1,5 @@
-// Функция, возвращающая случайное целое число из переданного диапазона включительно.
-
-function getRandomInt(min, max) {
-  if (isNaN(min) || isNaN(max) || isNaN(accuracy)) {
-    throw new Error('All parameters must be numbers');
-  }
-
-  if (min < 0 || max < 0) {
-    throw new Error('All  parameters must be positive');
-  }
-
-  if (min > max) {
-    throw new Error('First parameter must be smaller than the second');
-  }
-
-  return Math.floor( Math.random() * (max + min + 1) + min);
-}
-
-
-// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
-
-function getRandomFloat(min, max, accuracy) {
+// утилитарные функции
+function getRandomNumber(min, max, accuracy = 0) {
   if (isNaN(min) || isNaN(max) || isNaN(accuracy)) {
     throw new Error('All parameters must be numbers');
   }
@@ -29,7 +9,7 @@ function getRandomFloat(min, max, accuracy) {
   }
 
   if (min > max) {
-    throw new Error('First parameter must be smaller than the second');
+    [min, max] = [max, min];
   } else if (min === max) {
     return min;
   }
@@ -39,27 +19,151 @@ function getRandomFloat(min, max, accuracy) {
       throw new Error('The range is too small');
     }
 
-    let minDigits = String(min).split('.')[1];
-    let maxDigits = String(max).split('.')[1];
+    const MIN_DIGITS = String(min).split('.')[1];
+    const MAX_DIGITS = String(max).split('.')[1];
+    const MIN_LENGTH = Math.min(MIN_DIGITS.length, MAX_DIGITS.length, accuracy);
 
-    let minLength = minDigits.length < maxDigits.length ? minDigits : maxDigits;
-    minLength = minLength < accuracy ? minLength : accuracy;
-
-    for (let i = 0; i < minLength; i++) {
-      if (maxDigits[i] - minDigits[i] < 0) {
+    for (let i = 0; i < MIN_LENGTH; i++) {
+      if (MAX_DIGITS[i] - MIN_DIGITS[i] < 0) {
         throw new Error('The range is too small');
-      } else if (maxDigits[i] - minDigits[i] >= 1) {
+      } else if (MAX_DIGITS[i] - MIN_DIGITS[i] >= 1) {
         break
       }
 
-      if (i === minLength - 1 && maxDigits[i] - minDigits[i] < 1) {
+      if (i === MIN_LENGTH - 1 && MAX_DIGITS[i] - MIN_DIGITS[i] < 1) {
         throw new Error('The range is too small');
       }
     }
   }
 
-  let coefficient = 10 ** accuracy;
-  max = Math.floor(max * coefficient) / coefficient;
-  min = Math.ceil(min * coefficient) / coefficient;
+  const COEFFICIENT = 10 ** accuracy;
+  max = Math.floor(max * COEFFICIENT) / COEFFICIENT;
+  min = Math.ceil(min * COEFFICIENT) / COEFFICIENT;
   return Number((Math.random() * (max - min) + min).toFixed(accuracy));
 }
+
+function makeArray(start, stop) {
+  let array = [];
+  for (let i = start; i <= stop; i++) array.push(i);
+  return array;
+}
+
+function addInitialZeros(number, stringLength) {
+  number = String(number)
+  while (number.length < stringLength) number = '0' + number;
+  return number;
+}
+
+function getRandomArrayElement(array) {
+  return array[getRandomNumber(0, array.length - 1)];
+}
+
+function shuffle(iterable) {
+  for (let i = iterable.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [iterable[i], iterable[j]] = [iterable[j], iterable[i]];
+  }
+  return iterable;
+}
+
+function getRandomSubArray(array) {
+  const RANDOM_LENGTH = getRandomNumber(0, array.length - 1)
+  return shuffle(array).slice(RANDOM_LENGTH);
+}
+
+
+// константы
+const OFFERS_COUNT = 10;
+const AVATAR_LINK_TEMPLATE = 'img/avatars/user{{xx}}.png';
+const TITLES = [
+  'Уютная квартирка в центре города',
+  'Сдам дворец',
+];
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+];
+const TIME_OPTIONS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const DESCRIPTIONS = [
+  'Сдается уютная квартира в центре города, можно с котом, собак не люблю',
+  'Сдам дворец на выходные, порядочным славянам, уборка за ваш счет',
+];
+const PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
+
+// enum objects
+const Price = {
+  MIN: 100,
+  MAX: 10000000,
+};
+const Rooms = {
+  MIN: 1,
+  MAX: 50,
+};
+const Guests = {
+  MIN: 1,
+  MAX: 20,
+};
+const Location = {
+  X: {
+    MIN: 35.65000,
+    MAX: 35.70000,
+  },
+  Y: {
+    MIN: 139.70000,
+    MAX: 139.80000,
+  },
+  ACCURACY: 5,
+}
+
+// конструктор
+function createOffer(id) {
+  const LOCATION_X = getRandomNumber(Location.X.MIN, Location.X.MAX, Location.ACCURACY);
+  const LOCATION_Y = getRandomNumber(Location.Y.MIN, Location.Y.MAX, Location.ACCURACY);
+
+  return {
+    author: {
+      avatar: AVATAR_LINK_TEMPLATE.replace('{{xx}}', id),
+    },
+    offer: {
+      title: getRandomArrayElement(TITLES),
+      address: `${LOCATION_X}, ${LOCATION_Y}`,
+      price: getRandomNumber(Price.MIN, Price.MAX),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomNumber(Rooms.MIN, Rooms.MAX),
+      guests: getRandomNumber(Guests.MIN, Guests.MAX),
+      checkin: getRandomArrayElement(TIME_OPTIONS),
+      checkout: getRandomArrayElement(TIME_OPTIONS),
+      features: getRandomSubArray(FEATURES),
+      description: getRandomArrayElement(DESCRIPTIONS),
+      photos: getRandomSubArray(PHOTOS),
+    },
+    location: {
+      x: LOCATION_X,
+      y: LOCATION_Y,
+    },
+  };
+}
+
+
+const OFFERS = makeArray(1, OFFERS_COUNT)
+  .map((id) => addInitialZeros(id, String(OFFERS_COUNT).length))
+  .map((id) => createOffer(id));
